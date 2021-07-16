f4a31ef8db075ad24d5130b00a42ad69e65ede2a
Farhan Mustar <farhanmustar@gmail.com>
2021-07-16 21:05:36 +0800

    Initial commit.

---
+++ tutorial.py
@@ -0,0 +1,21 @@
+# Note taken from https://cht.sh/python/:learn
+
+# Single line comments start with a number symbol.
+
+""" Multiline strings can be written
+    using three "s, and are often used
+    as documentation.
+"""
+
+####################################################
+## 1. Primitive Datatypes and Operators
+####################################################
+
+# You have numbers
+3  # => 3
+
+# Math is what you would expect
+1 + 1   # => 2
+8 - 1   # => 7
+10 * 2  # => 20
+35 / 5  # => 7.0
